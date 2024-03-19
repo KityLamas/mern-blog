6ecd521668634eb1c7e5100448b8c5fff014ad85
Your Name <you@example.com>
2024-03-19 10:09:48 -0300

    create and add footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ import SignUp from './pages/SignUp'
 import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
 import Header from './components/Header'
-
+import Footer from './components/Footer'
 
 export default function App() {
   return (
@@ -20,14 +20,9 @@ export default function App() {
       <Route path='/sign-up' element={<SignUp/> }/>
       <Route path='/dashboard' element={<Dashboard/> }/>
       <Route path='/projects' element={<Projects/> }/>
-
-
-
-
-
-
     </Routes>
     
+    <Footer/>
     </BrowserRouter>
   )
 }
