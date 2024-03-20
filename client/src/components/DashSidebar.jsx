7a9875b434763419390390b4b2ec142eae89957f
Your Name <you@example.com>
2024-03-20 19:26:29 -0300

    components dash post dash comment

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -9,8 +9,37 @@ import {
 } from 'react-icons/hi';
 import { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import { signoutSuccess } from '../redux/user/userSlice';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 
-function DashSidebar() {
+export default function DashSidebar() {
+  const location = useLocation();
+  const dispatch = useDispatch();
+  const { currentUser } = useSelector((state) => state.user);
+  const [tab, setTab] = useState('');
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get('tab');
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
+  const handleSignout = async () => {
+    try {
+      const res = await fetch('/api/user/signout', {
+        method: 'POST',
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        dispatch(signoutSuccess());
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <Sidebar className='w-full md:w-56'>
       <Sidebar.Items>
@@ -80,7 +109,5 @@ function DashSidebar() {
         </Sidebar.ItemGroup>
       </Sidebar.Items>
     </Sidebar>
-  )
+  );
 }
-
-export default DashSidebar
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,8 +2,12 @@ import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import DashSidebar from '../components/DashSidebar';
 import DashProfile from '../components/DashProfile';
+import DashPosts from '../components/DashPosts';
+import DashUsers from '../components/DashUsers';
+import DashComments from '../components/DashComments';
+import DashboardComp from '../components/DashboardComp';
 
-function Dashboard() {
+export default function Dashboard() {
   const location = useLocation();
   const [tab, setTab] = useState('');
   useEffect(() => {
@@ -15,18 +19,20 @@ function Dashboard() {
   }, [location.search]);
   return (
     <div className='min-h-screen flex flex-col md:flex-row'>
-      <div>
-
-        
+      <div className='md:w-56'>
+        {/* Sidebar */}
+        <DashSidebar />
       </div>
-
-    <div>
-
-      
+      {/* profile... */}
+      {tab === 'profile' && <DashProfile />}
+      {/* posts... */}
+      {tab === 'posts' && <DashPosts />}
+      {/* users */}
+      {tab === 'users' && <DashUsers />}
+      {/* comments  */}
+      {tab === 'comments' && <DashComments />}
+      {/* dashboard comp */}
+      {tab === 'dash' && <DashboardComp />}
     </div>
-
-    </div>
-  )
+  );
 }
-
-export default Dashboard
